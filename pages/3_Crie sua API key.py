838a92435662a281c903797f4d7cadeca26c44e6
Rosapedroamaral <166629400+Rosapedroamaral@users.noreply.github.com>
2024-04-15 15:09:13 -0300

    Pasta contendo as outras paginas da aplicação
    Com essa pasta o site pode possuir um menu

---
+++ pages/3_Crie sua API key.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from PIL import Image
+import cv2
+# Page title
+st.set_page_config(page_title='Crie sua API key ', page_icon='🤖')
+st.title('🤖 Como criar sua API key?')
+
+
+img = Image.open("descbot.jpg")
+st.image(
+    img,
+    caption = "Olá!",
+    width = 400,
+    channels = "RGB"
+)
+st.write(
+    '''
+    Crie uma Conta no ChatPDF: Primeiro, acesse o site do ChatPDF e crie uma conta. Se você já tiver uma conta, faça login.
+    Obtenha sua Chave de API:
+    Após fazer login, vá para a seção “My Account” (Minha Conta).
+    Expanda as configurações de desenvolvedor (Developer settings).
+    Lá, você encontrará sua chave de API (API key).
+    '''
+)
---
+++ pages/Sobre os Devs.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write(
+    "Somos alunos da Universidade do Estado do Rio de Janeiro (UERJ). E desenvolvemos esse chatbot para aa disciplina Projetos de Ambiente Computacional"
+)
