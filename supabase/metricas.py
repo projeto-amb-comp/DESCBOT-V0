2a89e5185b6d50e380a367084db7712fe3316dfd
Vinicius <salazar.vncs@gmail.com>
2024-05-29 12:14:37 -0300

    Adição do método 'consulta_registro', validação de email por string e adição da coluna 'Requisicoes' na tabela 'Metricas'

--- supabase/metricas.py
+++ supabase/metricas.py
@@ -2,33 +2,58 @@ from supabase import create_client
 
 class MetricasClient:
     def __init__(self, supabase_client):
+        """
+        Inicializa o cliente de métricas com uma instância do cliente Supabase.
+        """
         self.supabase_client = supabase_client
 
     def insere_id(self, user_id):
-        data = {"ID": user_id, "Acessos": 0, "Alteracoes": 0}
+        """
+        Insere um novo ID na tabela de métricas com valores iniciais para 'Acessos', 'Alteracoes' e 'Requisicoes'.
+        """
+        data = {"ID": user_id, "Acessos": 0, "Alteracoes": 0, "Requisicoes": 0}
         self.supabase_client.client.table("Metricas").insert(data).execute()
 
     def atualizar_acessos(self, user_id):
+        """
+        Incrementa o número de acessos para o usuário com o ID fornecido.
+        """
         # Obter o valor atual de "Acessos" para o usuário com o ID fornecido
         response = self.supabase_client.client.table("Metricas").select("Acessos").eq("ID", user_id).execute()
         acessos_atuais = response.data[0]["Acessos"] if response.data else 0
 
         # Atualizar o registro incrementando "Acessos"
         data = {"ID": user_id, "Acessos": acessos_atuais + 1}
-        response = self.supabase_client.client.table("Metricas").upsert(data).execute()
-        return response
-
+        self.supabase_client.client.table("Metricas").upsert(data).execute()
 
     def atualizar_alteracoes(self, user_id):
+        """
+        Incrementa o número de alterações e acessos para o usuário com o ID fornecido.
+        """
         # Obter o valor atual de "Alteracoes" e "Acessos" para o usuário com o ID fornecido
         response = self.supabase_client.client.table("Metricas").select("Alteracoes", "Acessos").eq("ID", user_id).execute()
         alteracoes_atuais = response.data[0]["Alteracoes"] if response.data else 0
         acessos_atuais = response.data[0]["Acessos"] if response.data else 0
 
-        # Atualizar o registro incrementando "Alteracoes"
+        # Atualizar o registro incrementando "Alteracoes" e "Acessos"
         data = {"ID": user_id, "Acessos": acessos_atuais + 1, "Alteracoes": alteracoes_atuais + 1}
-        response = self.supabase_client.client.table("Metricas").upsert(data).execute()
-        return response
+        self.supabase_client.client.table("Metricas").upsert(data).execute()
+
+    def atualizar_requisicoes(self, user_id):
+        """
+        Incrementa o número de requisições e acessos para o usuário com o ID fornecido.
+        """
+        # Obter o valor atual de "Requisicoes" e "Acessos" para o usuário com o ID fornecido
+        response = self.supabase_client.client.table("Metricas").select("Requisicoes", "Acessos").eq("ID", user_id).execute()
+        requisicoes_atuais = response.data[0]["Requisicoes"] if response.data else 0
+        acessos_atuais = response.data[0]["Acessos"] if response.data else 0
+
+        # Atualizar o registro incrementando "Requisicoes" e "Acessos"
+        data = {"ID": user_id, "Acessos": acessos_atuais + 1, "Requisicoes": requisicoes_atuais + 1}
+        self.supabase_client.client.table("Metricas").upsert(data).execute()
 
     def deleta_id(self, user_id):
+        """
+        Deleta o registro de métricas para o usuário com o ID fornecido.
+        """
         self.supabase_client.client.table("Metricas").delete().eq("ID", user_id).execute()
--- supabase/registros.py
+++ supabase/registros.py
@@ -1,6 +1,5 @@
 import os
 import random
-import string
 from supabase import create_client, Client
 from dotenv import load_dotenv
 from metricas import MetricasClient
@@ -19,104 +18,156 @@ class SupabaseClient:
         self.metricas_client = MetricasClient(self)
 
     def gera_id(self):
+        # Gera um ID único
         while True:
             id = random.randint(1, 1000)
             n = self.client.table("Registros").select("ID").eq("ID", id).execute()
             if len(n.data) == 0:
                 return id
-          
-    def gera_api_key(self):
-        while True:
-            api_key_length = random.randint(5, 20)
-            characters = string.ascii_letters + string.digits
-            api_key = ''.join(random.choice(characters) for _ in range(api_key_length))
-            n = self.client.table("Registros").select("APIKey").eq("APIKey", api_key).execute()
-            if len(n.data) == 0:
-                return api_key
 
-    def insere_dados(self, nome, email, senha):
-        # Verificar se o email já está cadastrado
+    def valida_email(self, email):
+        # Verifica se o email contém "@" e "."
+        if "@" in email and "." in email and email.count("@") == 1:
+            username, domain = email.split("@")
+            # Verifica se há pelo menos um caractere antes e depois do "@", e se termina com ".com" ou ".com.br"
+            if len(username) > 0 and domain.count(".") >= 1:
+                # Verifica se há pelo menos uma letra após o "@" e antes do ponto "."
+                parts = domain.split(".")
+                if len(parts[0]) > 0 and parts[0].isalpha():
+                    return True
+        return False
+
+    def insere_dados(self, nome, email, senha, apikey):
+        # Verifica se o email é válido
+        if not self.valida_email(email):
+            print("Email inválido")
+            return
+
+        # Verifica se o email já está cadastrado
         result = self.client.table("Registros").select("ID").eq("Email", email).execute()
         if len(result.data) > 0:
             print("Email já cadastrado")
             return
-        
-        # Gerar um novo ID e APIKey únicos
-        id = self.gera_id()
-        api_key = self.gera_api_key()
 
-        data = {"ID": id, "Nome": nome, "Email": email, "Senha": senha, "APIKey": api_key}
+        # Gera um novo ID e APIKey únicos
+        id = self.gera_id()
+
+        data = {"ID": id, "Nome": nome, "Email": email, "Senha": senha, "APIKey": apikey}
         self.client.table("Registros").insert(data).execute()
 
-        # Inserir o ID na tabela Metricas
+        # Insere o ID na tabela Metricas
         self.metricas_client.insere_id(id)
 
         print("Registro inserido com sucesso.")
 
-    def deleta_dados(self, email, senha):
-        # Verificar se o email está cadastrado
-        email_result = self.client.table("Registros").select("ID", "Senha").eq("Email", email).execute()
-        if len(email_result.data) == 0:
-            print("Email não cadastrado")
-            return
-        
-        # Verificar se a senha corresponde ao ID do email
-        record = email_result.data[0]
-        if record["Senha"] != senha:
-            print("senha incorreta")
-            return
-        
-        # Deletar a linha se a senha corresponder
-        record_id = record["ID"]
-        self.client.table("Registros").delete().eq("ID", record_id).execute()
-
-        # Deletar o ID na tabela Metricas
-        self.metricas_client.deleta_id(record_id)
-
-        print(f"Registro deletado com sucesso para o email {email}")
-
     def atualiza_dados(self, email, senha):
-        # Verificar se o email está cadastrado e a senha está correta
+        # Verifica se o email é válido
+        if not self.valida_email(email):
+            print("Email inválido")
+            return
+
+        # Verifica se o email está cadastrado e a senha está correta
         result = self.client.table("Registros").select("ID", "Nome", "Email", "Senha", "APIKey").eq("Email", email).execute()
         if len(result.data) == 0 or result.data[0]["Senha"] != senha:
             print("Email ou senha inválidos")
             return
-        
-        # Pedir novos dados ao usuário
-        novo_nome = input("Insira o novo nome: ")
-        novo_email = input("Insira o novo email: ")
-        nova_senha = input("Insira a nova senha: ")
-        novo_apikey = input("Insira seu APIKey: ")
-        
-        # Atualizar o registro mantendo o mesmo ID e APIKey
+
         record = result.data[0]
-        data = {
-            "Nome": novo_nome,
-            "Email": novo_email,
-            "Senha": nova_senha,
-            "APIKey": novo_apikey
-        }
-        self.client.table("Registros").update(data).eq("ID", record["ID"]).execute()
+        dados_novos = {}
 
-        # Atualizar a tabela Metricas
-        self.metricas_client.atualizar_alteracoes(record["ID"])
+        while True:
+            print("Qual dado você deseja modificar?")
+            print("1. Nome")
+            print("2. Email")
+            print("3. Senha")
+            print("4. APIKey")
+            print("5. Atualizar e sair")
+            opcao = input("Escolha uma opção: ")
 
-        print("Dados atualizados com sucesso.")
+            if opcao == "1":
+                novo_nome = input("Insira o novo nome: ")
+                dados_novos["Nome"] = novo_nome
+            elif opcao == "2":
+                novo_email = input("Insira o novo email: ")
+                if self.valida_email(novo_email):
+                    dados_novos["Email"] = novo_email
+                else:
+                    print("Email inválido")
+            elif opcao == "3":
+                nova_senha = input("Insira a nova senha: ")
+                dados_novos["Senha"] = nova_senha
+            elif opcao == "4":
+                novo_apikey = input("Insira seu novo APIKey: ")
+                dados_novos["APIKey"] = novo_apikey
+            elif opcao == "5":
+                break
+            else:
+                print("Opção inválida. Tente novamente.")
+
+        if dados_novos:
+            target_email = dados_novos.get("Email", email)
+            id_result = self.client.table("Registros").select("ID").eq("Email", target_email).execute()
+            if len(id_result.data) == 0 or (target_email == email):
+                self.client.table("Registros").update(dados_novos).eq("ID", record["ID"]).execute()
+                self.metricas_client.atualizar_alteracoes(record["ID"])
+                print("Dados atualizados com sucesso.")
+            else:
+                print("Email já cadastrado")
+        else:
+            print("Nenhuma alteração feita.")
+
+    def deleta_dados(self, email, senha):
+        # Verifica se o email está cadastrado
+        email_result = self.client.table("Registros").select("ID", "Senha").eq("Email", email).execute()
+        if len(email_result.data) == 0:
+            print("Email não cadastrado")
+            return
+
+        # Verifica se a senha corresponde ao ID do email
+        record = email_result.data[0]
+        if record["Senha"] != senha:
+            print("Senha incorreta")
+            return
+
+        # Deleta a linha se a senha corresponder
+        record_id = record["ID"]
+        self.client.table("Registros").delete().eq("ID", record_id).execute()
+
+        # Deleta o ID na tabela Metricas
+        self.metricas_client.deleta_id(record_id)
+
+        print(f"Registro deletado com sucesso para o email {email}")
+
+    def consulta_registro(self, email, senha):
+        # Consulta um registro existente na tabela "Registros"
+        result = self.client.table("Registros").select("ID", "Nome", "Email", "Senha", "APIKey").eq("Email", email).execute()
+        if len(result.data) == 0 or result.data[0]["Senha"] != senha:
+            print("Dados inválidos")
+            return
+
+        record = result.data[0]
+        print(f"Nome: {record['Nome']}")
+        print(f"Email: {record['Email']}")
+        print(f"Senha: {record['Senha']}")
+        print(f"APIKey: {record['APIKey']}")
+
+        # Atualiza a tabela Metricas
+        self.metricas_client.atualizar_requisicoes(record["ID"])
 
     def autentica_dados(self, email, senha):
-        # Verificar se o email está cadastrado
+        # Autentica um registro existente na tabela "Registros"
         result = self.client.table("Registros").select("ID", "Senha").eq("Email", email).execute()
         if len(result.data) == 0:
             print("Usuário não cadastrado")
             return False
-        
-        # Verificar se a senha corresponde ao email
+
+        # Verifica se a senha corresponde ao email
         record = result.data[0]
         if record["Senha"] != senha:
             print("Senha inválida")
             return False
-        
-        # Atualizar a tabela Metricas
+
+        # Atualiza a tabela Metricas
         self.metricas_client.atualizar_acessos(record["ID"])
 
         print("Acesso liberado")
@@ -130,38 +181,46 @@ def menu_principal():
         print("2. Atualizar registro")
         print("3. Deletar registro")
         print("4. Autenticar registro")
-        print("5. Sair")
-        
+        print("5. Consultar registro")
+        print("6. Sair")
+
         opcao = input("Escolha uma opção: ")
 
-        print("-"*40)
-        
+        print("-" * 40)
+
         if opcao == "1":
             nome = input("Insira o nome: ")
             email = input("Insira o email: ")
             senha = input("Insira a senha: ")
+            apikey = input("Insira a apikey: ")
 
-            supabase_client.insere_dados(nome, email, senha)
+            supabase_client.insere_dados(nome, email, senha, apikey)
 
         elif opcao == "2":
             email = input("Insira o email: ")
             senha = input("Insira a senha: ")
 
             supabase_client.atualiza_dados(email, senha)
-                    
+
         elif opcao == "3":
             email = input("Digite seu email: ")
             senha = input("Digite sua senha: ")
-            
+
             supabase_client.deleta_dados(email, senha)
 
         elif opcao == "4":
             email = input("Digite seu email: ")
             senha = input("Digite sua senha: ")
-            
+
             supabase_client.autentica_dados(email, senha)
 
         elif opcao == "5":
+            email = input("Digite seu email: ")
+            senha = input("Digite sua senha: ")
+
+            supabase_client.consulta_registro(email, senha)
+
+        elif opcao == "6":
             print("Saindo...")
             break
         else:
